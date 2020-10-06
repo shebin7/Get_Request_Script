b495f2f3cbb6fa9720476251efd30f986dd0a4b6
shebin7 <shebinsaji7@outlook.com>
2020-10-06 13:35:13 +0530

    Modified script Get_api_result

--- Get_api_result.py
+++ Get_api_result.py
@@ -1,18 +1,25 @@
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from rich.console import Console
+from rich.table import Table
 from rich.panel import Panel
-
 import requests
 import json
 import csv
 import time
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import readline
 
+
 console = Console()
+module_table =Table(title="API module Table")
+module_table.add_column('Modules',style='yellow bold')
+module_table.add_column('Vendors',style='blue bold')
+module_table.add_column("Description",style='purple bold')
+
 
 global user_module
 
-class AutoComplete(object):  # Custom completer #
+### Custom Autocomplete Class for completing users input ###
+class AutoComplete(object):  
 
     def __init__(self, options):
         self.options = sorted(options)
@@ -39,15 +46,20 @@ completer = AutoComplete(["get_interfaces","get_eigrp","get_cdp","get_ospf"])
 readline.set_completer(completer.complete)
 readline.parse_and_bind('tab: complete')
 
+module_table.add_row('get_ospf','Cisco',"Gets configuration of OSPF")
+module_table.add_row('get_interfaces','IETF/Open','Gets all interfaces details')
+console.print(module_table)
+console.print('\n')
 console.print("Press [bold][red]<TAB>[/bold][/red] to autocomplete words",style='yellow bold')
-user_module= input("Input: ")
-console.print("You entered", user_module)
+console.print('\n')
+user_module= input("Please Enter the API module you want to retrieve =>")
+console.print("You entered the following->"+user_module,style='bold green')
 
 
 def Rest_Get():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-    get_module_csv = '/home/shebin/NETDEVOPS/Net_automation_Project/RESTCONF/Adding_and_Configuring_ Int/get_module.csv'
+    get_module_csv = '/home/shebin/NETDEVOPS/Net_automation_Project/RESTCONF/Get_Request_Script/get_module.csv'
 
     main_url = 'https://ios-xe-mgmt-latest.cisco.com:9443/restconf/'
 
@@ -70,13 +82,13 @@ def Rest_Get():
                     
         response = requests.get(url=url,auth=("developer","C1sco12345"),verify=False,headers=headers).json()
         final_result = json.dumps(response,indent=2,sort_keys=True)
-        console.print('='*30)
+        console.print('='*40)
         console.print(final_result,style='bold blue')
     except NameError as nr:
         console.print("The API you entered not listed in our database",style='bold red')
     
     finally:
-        console.print('='*30)
+        console.print('='*40)
         console.print("Execution completed...",style='bold yellow')
         console.print('\n')
         time.sleep(1)
@@ -85,39 +97,3 @@ def Rest_Get():
     
 
 Rest_Get()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-c= ['sam','dam','ram','tam']
-
-
-for s in c:
-    d = c
-    print(d)
-r = d[0]
-print(r)
-'''
