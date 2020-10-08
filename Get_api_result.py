d2442775175501bfdab26bd12527208c87fedf9b
shebin7 <shebinsaji7@outlook.com>
2020-10-08 14:24:29 +0530

    Modified Code

--- Get_api_result.py
+++ Get_api_result.py
@@ -1,4 +1,5 @@
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from json.decoder import JSONDecodeError
 from rich.console import Console
 from rich.table import Table
 from rich.panel import Panel
@@ -9,14 +10,35 @@ import time
 import readline
 
 
+
+
+restconf_api_module = '/home/shebin/NETDEVOPS/Net_automation_Project/RESTCONF/Get_Request_Script/RESTCONF_API_MODULES_FOR_TABLE.csv'
+
+global user_module
+global module_list
+
+
+module_list=[]
 console = Console()
 module_table =Table(title="API module Table")
+
 module_table.add_column('Modules',style='yellow bold')
 module_table.add_column('Vendors',style='blue bold')
 module_table.add_column("Description",style='purple bold')
+module_table.add_column("Platform",style='bold yellow')
+
+with open(restconf_api_module,'r') as mr:
+    module_read = csv.DictReader(mr) 
+    for row in module_read:
+        module_name = row['Modules']   
+        vendor_name = row['Vendors'] 
+        Desc        = row['Description'] 
+        Platform    = row['Platform'] 
+        module_table.add_row(module_name,vendor_name,Desc,Platform) 
+        module_list.append(module_name) 
 
 
-global user_module
+
 
 ### Custom Autocomplete Class for completing users input ###
 class AutoComplete(object):  
@@ -42,7 +64,7 @@ class AutoComplete(object):
 
         
 
-completer = AutoComplete(["get_interfaces","get_eigrp","get_cdp","get_ospf"])
+completer = AutoComplete(module_list)
 readline.set_completer(completer.complete)
 readline.parse_and_bind('tab: complete')
 
@@ -58,6 +80,7 @@ console.print("You entered the following module->"+user_module,style='bold purpl
 time.sleep(0.5)
 
 
+### This function will execute the request by user and will return Results ###
 def Rest_Get():
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -88,11 +111,32 @@ def Rest_Get():
         console.print('='*40)
         time.sleep(1)
         console.print(final_result,style='bold blue')
-    except NameError as nr:
+    except requests.exceptions.ConnectionError as ec:
+        console.print("See whether you have connectivity with the end device",ec,style='bold red')
+    except requests.exceptions.HTTPError as eh:
+        console.print("Bad Request specify correct Requests",eh,style='bold red')
+        console.print("Check  whether you correctly specified  http/https or else Check Authentication parameters",style='bold yellow')
+        console.print(eh.response.text,style='bold green')
+    except requests.exceptions.InvalidURL as eiu:
+        console.print('URL specified is not a valid one',eiu,style='bold red')
+    except requests.exceptions.URLRequired as eur:
+        console.print("Please specify a URL for request",eur,style='bold red')
+    except requests.exceptions.InvalidSchema as esc:
+        console.print("Please use either 'HTTP' or 'HTTPS'",esc,style='bold red')
+    except requests.exceptions.ContentDecodingError as edc:
+        console.print('Please specify correct Encodings',edc,style='bold red')
+    except requests.exceptions.RequestException as erq:
+        console.print("Sorry not able to rectify the issue,Please ",erq,style='bold red')
+    except ValueError:
+        console.print('No content available for this request',style='bold red')
+        console.print("Device not able to return anything,since no configuration found on the device for requested API",style='bold red')
+    except NameError:
         console.print("The API you entered not listed in our database",style='bold red')
     except Exception:
-        console.print("Device not able to return anything,since no configuration found on the device for requested API",style='bold red')
-    
+        console.print("Sorry some other issues occured..Please look into it")
+
+
+        
     finally:
         console.print('='*40)
         console.print("Execution completed...",style='bold yellow')
